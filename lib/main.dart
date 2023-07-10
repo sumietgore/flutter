1c116c36106eb59dfec71b446c1003a550a38197
Sumiet Gore <sumeetgore96@gmail.com>
2023-07-10 15:22:55 +0200

    Curpentino Scroll Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:window_manager/window_manager.dart';
 
 void main() async {
@@ -19,52 +20,30 @@ void main() async {
     await windowManager.focus();
   });
 
-  runApp(const MyApp());
+  runApp(const VendingMachineApp());
 }
 
-class MyCustomScrollBehavior extends MaterialScrollBehavior {
-  // Override behavior methods and getters like dragDevices
-  @override
-  Set<PointerDeviceKind> get dragDevices => { 
-    PointerDeviceKind.touch,
-    PointerDeviceKind.mouse,
-    // etc.
-  };
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class VendingMachineApp extends StatelessWidget {
+  const VendingMachineApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TuroBox Vend',
-      scrollBehavior: MyCustomScrollBehavior(),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        useMaterial3: false,
-      ),
-      home: const MyHomePage(title: 'TuroBox Home'),
+    return const MaterialApp(
+      title: 'Vending Machine',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class HomePage extends StatelessWidget { 
+  const HomePage({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(80), // Set the desired height here
+        preferredSize: const Size.fromHeight(80),
         child: Container(
           color: Colors.white,
           padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -79,87 +58,117 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(10),
-        children: const [
+      body: CupertinoScrollbar(
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: const [
           ProductCard(
+        
             productName: 'Coke',
             productPrice: 20,
             productImage: 'assets/products/Coke.png',
+            productDescription: 'Coke',
           ),
           ProductCard(
+          
             productName: 'Coke (Bottle)',
             productPrice: 20,
             productImage: 'assets/products/CokeBottle.png',
+            productDescription: 'Coke Bottle',
           ),
           ProductCard(
+         
             productName: 'Coke (Can)',
             productPrice: 40,
             productImage: 'assets/products/CokeCan.png',
+          productDescription: 'Coke Can',
           ),
           ProductCard(
+        
             productName: 'Coke Zero',
             productPrice: 20,
             productImage: 'assets/products/CokeZero.png',
+          productDescription: 'Coke Zero',
           ),
           ProductCard(
+            
             productName: 'Coke Zero (Bottle)',
             productPrice: 20,
             productImage: 'assets/products/CokeZeroBottle.png',
+          productDescription: 'Coke Zero Bottle',
           ),
           ProductCard(
+           
             productName: 'Coke Zero (Can)',
             productPrice: 40,
             productImage: 'assets/products/CokeZeroCan.png',
+          productDescription: 'Coke Zero Can',
           ),
           ProductCard(
+          
             productName: 'Fanta',
             productPrice: 20,
             productImage: 'assets/products/Fanta.png',
+          productDescription: 'Fanta',
           ),
           ProductCard(
+            key:Key('FantaBottle'),
             productName: 'Fanta (Bottle)',
             productPrice: 20,
             productImage: 'assets/products/FantaBottle.png',
+          productDescription: 'Fanta Bottle',
           ),
           ProductCard(
+       
             productName: 'Fanta (Can)',
             productPrice: 40,
             productImage: 'assets/products/FantaCan.png',
+          productDescription: 'Fanta Can',
           ),
           ProductCard(
+          
             productName: 'Pepsi',
             productPrice: 20,
             productImage: 'assets/products/Pepsi.png',
+          productDescription: 'Pepsi',
           ),
           ProductCard(
+       
             productName: 'Pepsi (Bottle)',
             productPrice: 20,
             productImage: 'assets/products/PepsiBottle.png',
+          productDescription: 'Pepsi Bottle',
           ),
           ProductCard(
+           
             productName: 'Pepsi (Can)',
             productPrice: 40,
             productImage: 'assets/products/PepsiCan.png',
+          productDescription: 'Pepsi Can',
           ),
           ProductCard(
+          
             productName: 'Sprite',
             productPrice: 20,
             productImage: 'assets/products/Sprite.png',
+          productDescription: 'Sprite',
           ),
           ProductCard(
+          
             productName: 'Sprite (Bottle)',
             productPrice: 20,
             productImage: 'assets/products/SpriteBottle.png',
+          productDescription: 'Sprite Bottle',
           ),
           ProductCard(
+        
             productName: 'Sprite (Can)',
             productPrice: 40,
             productImage: 'assets/products/SpriteCan.png',
+          productDescription: 'Sprite Can',
           ),
-          // Add more ProductCard widgets for other products
         ],
+        ),
       ),
     );
   }
@@ -169,12 +178,14 @@ class ProductCard extends StatelessWidget {
   final String productName;
   final double productPrice;
   final String productImage;
+  final String productDescription;
 
   const ProductCard({
     super.key,
     required this.productName,
     required this.productPrice,
     required this.productImage,
+    required this.productDescription
   });
 
   @override
@@ -203,7 +214,7 @@ class ProductCard extends StatelessWidget {
             ),
             const SizedBox(height: 5),
             Text(
-              '₹${productPrice.toStringAsFixed(2)}',
+              '₹ ${productPrice.toStringAsFixed(2)}',
               style: const TextStyle(
                 fontSize: 14,
               ),
@@ -221,14 +232,48 @@ class ProductCard extends StatelessWidget {
         return Container(
           height: 500,
           color: Colors.white,
-          child: const Center(
-            child: Text(
-              'Product Details',
-              style: TextStyle(fontSize: 20),
-            ),
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                productName,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Price: \$${productPrice.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                productDescription,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement the logic to add the product to the cart
+                  _addToCart();
+                  Navigator.of(context).pop(); // Close the bottom sheet
+                },
+                child: const Text('Add to Cart'),
+              ),
+              // Add more product details as needed
+            ],
           ),
         );
       },
     );
   }
+
+  void _addToCart() {
+    // Implement the logic to add the product to the cart
+    // This method will be called when the "Add to Cart" button is pressed
+  }
 }
+
+
