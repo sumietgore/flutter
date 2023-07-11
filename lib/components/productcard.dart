05ba41d115687b86f5fa5d6251ca5f43d1efbdc0
Sumiet Gore <sumeetgore96@gmail.com>
2023-07-11 14:07:38 +0200

    Created seperate file for ProductCard component

---
+++ lib/components/productcard.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String productName;
+  final double productPrice;
+  final String productImage;
+  final String productDescription;
+
+  const ProductCard(
+      {super.key,
+      required this.productName,
+      required this.productPrice,
+      required this.productImage,
+      required this.productDescription});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Card(
+        color: Colors.grey.shade100,
+        elevation: 0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              productImage,
+              width: 120,
+              height: 120,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              productName,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              '₹ ${productPrice.toStringAsFixed(2)}',
+              style: const TextStyle(
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
